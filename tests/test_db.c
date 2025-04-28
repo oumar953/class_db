05b858a73bc41bcd9ce65be9e5c186be4a90ccd2
oumar953 <koneoumar.p@gmail.com>
2025-04-28 20:00:05 +0200

    Ajout des tests  pour la BDD ((INSERT, SELECT, DELETE, save,load)

--- tests/test_db.c
+++ tests/test_db.c
@@ -8,13 +8,34 @@ int main() {
     Table table;
     init_table(&table);
 
-    insert_row(&table, 1, "Alice");
-    insert_row(&table, 2, "Bob");
+// table du début
+    insert_row(&table, 1, "Astan");
+    insert_row(&table, 2, "oumar");
 
     assert(table.row_count == 2);
     assert(table.rows[0].id == 1);
-    assert(strcmp(table.rows[1].name, "Bob") == 0);
+    assert(strcmp(table.rows[1].name, "oumar") == 0);
 
-    printf("All tests passed!\n");
+
+
+
+// test pour la supression
+    assert(delete_row(&table, 1) == 0);
+    assert(table.row_count == 1);
+    assert(table.rows[0].id == 2); // oumar devrait etre le seul restant
+
+    // Tests pour la sauvegarde
+    assert(save_table(&table, "data/test_db.dat") == 0);
+
+    Table loaded_table;
+    init_table(&loaded_table);
+    assert(load_table(&loaded_table, "data/test_db.dat") == 0);
+    assert(loaded_table.row_count == 1);
+    assert(loaded_table.rows[0].id == 2);
+    assert(strcmp(loaded_table.rows[0].name, "oumar") == 0);
+
+
+        printf("Tous les tests ont réussi! \n");
     return 0;
+
 }
